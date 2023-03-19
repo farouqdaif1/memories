44e3520258f062f631d95d628e3edf751307aeeb
farouqdaif1 <daiffarouq@gmail.com>
2023-03-19 15:50:35 +0200

    Make form to make a post

--- client/src/App.js
+++ client/src/App.js
@@ -1,35 +1,42 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Container, AppBar, Typography, Grow, Grid } from '@mui/material';
 import memories from './images/memories.png';
 import Posts from "./components/Posts/Posts";
 import Form from "./components/Form/Form";
 import useStyles from './styles';
-import { ThemeProvider} from '@mui/styles'
+import { ThemeProvider } from '@mui/styles'
 import { createTheme } from '@mui/material'
+import { useDispatch } from 'react-redux';
+import { getPosts } from './actions/posts'
+
 const App = () => {
     const classes = useStyles();
     const theme = createTheme();
+    const dispatch = useDispatch();
+    useEffect(() => {
+        dispatch(getPosts());
+    }, [dispatch]);
     return (
         <ThemeProvider theme={theme}>
-        <Container maxWidth="lg">
-            <AppBar className={classes.appBar} position="static" color="inherit" sx={{ flexDirection: 'row' }}>
-                <Typography className={classes.heading} variant="h2" align="center" > Memories</Typography>
-                <img className={classes.image} src={memories} alt="memories" height="60" />
-            </AppBar>
-            <Grow in>
-                <Grid container justify="space-between" alignItems="stretch" spacing={3}>
-                    <Grid item xs={12} sm={7}>
-                        <Posts />
+            <Container maxWidth="lg">
+                <AppBar className={classes.appBar} position="static" color="inherit" sx={{ flexDirection: 'row' }}>
+                    <Typography className={classes.heading} variant="h2" align="center" > Memories</Typography>
+                    <img className={classes.image} src={memories} alt="memories" height="60" />
+                </AppBar>
+                <Grow in>
+                    <Grid container justify="space-between" alignItems="stretch" spacing={3}>
+                        <Grid item xs={12} sm={7}>
+                            <Posts />
+                        </Grid>
+                        <Grid item xs={12} sm={4}>
+                            <Form />
+                        </Grid>
                     </Grid>
-                    <Grid item xs={12} sm={4}>
-                        <Form />
-                    </Grid>
-                </Grid>
 
-            </Grow>
+                </Grow>
 
             </Container>
-            </ThemeProvider>
+        </ThemeProvider>
 
     )
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
-import {StrictMode} from 'react';
-import {createRoot} from 'react-dom/client';
+import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import { Provider } from 'react-redux';
+import { configureStore } from '@reduxjs/toolkit'
+import thunk from 'redux-thunk';
+import reducers from './reducers';
 import App from './App';
+import './index.css';
+const store = configureStore({
+  reducer: reducers,
+  middleware: [thunk],
+})
+
 const rootElement = document.getElementById('root');
 const root = createRoot(rootElement);
 root.render(
-    <StrictMode>
+  <StrictMode>
+    <Provider store={store}>
       <App />
-    </StrictMode>,
-  );
+    </Provider>
+  </StrictMode>,
+);
 
