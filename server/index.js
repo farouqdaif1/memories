0f8a9e8caeb9aeb4f86cacde7a78866af9bb040a
farouqdaif1 <daiffarouq@gmail.com>
2023-03-19 15:49:59 +0200

    Make form to make a post

--- server/index.js
+++ server/index.js
@@ -1,19 +1,25 @@
-import express from "express";
-import bodyParser from "body-parser";
-import mongoose from "mongoose";
+
+import express from 'express';
+import bodyParser from 'body-parser';
+import mongoose from 'mongoose';
 import cors from 'cors';
 
 import postRoutes from './routes/posts.js';
 
 const app = express();
-app.use(bodyParser.json({ limit: "30mb", extended: true }));// we will send images have big size 
-app.use(bodyParser.urlencoded({ limit: "30mb", extended: true }));// we will send images have big size 
+
+app.use(bodyParser.json({ limit: '30mb', extended: true }))
+app.use(bodyParser.urlencoded({ limit: '30mb', extended: true }))
 app.use(cors());
 
 app.use('/posts', postRoutes);
 
-const CONNECTION_URL = 'mongodb+srv://farouqdaif:farouqdaif63@cluster0.2mxgzor.mongodb.net/?retryWrites=true&w=majority'
-const PORT = process.env.PORT || 5000;
+const CONNECTION_URL = 'mongodb+srv://memoriesadmin:memories@memories.wl8exjp.mongodb.net/?retryWrites=true&w=majority';
+const PORT = process.env.PORT|| 5000;
+
 mongoose.connect(CONNECTION_URL)
-    .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
-    .catch((error) => console.log(error.message));
+  .then(() => app.listen(PORT, () => console.log(`Server Running on Port: http://localhost:${PORT}`)))
+  .catch((error) => console.log(`${error} did not connect`));
+
+// mongoose.set('useFindAndModify', false);
+
